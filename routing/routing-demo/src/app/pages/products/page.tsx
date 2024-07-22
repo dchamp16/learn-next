844ae631b0a80a6be9a092609c940cf459775d1c
dchamp16 <dchamp16@gmail.com>
2024-07-22 09:42:32 -0600

    update

--- routing/routing-demo/src/app/pages/products/page.tsx
+++ routing/routing-demo/src/app/pages/products/page.tsx
@@ -1,24 +1,41 @@
-'use client'
+// 'use client'
+import { Metadata } from "next";
+
+type Props = {
+    params: {
+        productId: string;
+    };
+};
+
+export const generateMetadata = ({ params }: Props): Metadata => {
+    return {
+        title: `Product ${params.productId}`,
+    };
+};
 
 import Link from "next/link";
-import {usePathname} from "next/navigation";
-import {products} from "@/app/pages/products/[productId]/products";
+import { usePathname } from "next/navigation";
+import { products } from "@/app/pages/products/[productId]/products";
 
+export default function Products({ params }: Props) {
+    const pathname = usePathname();
+    console.log(pathname + '/1');
 
-export default function Products() {
-    console.log(usePathname() + '/1')
     return (
         <>
-            <nav
-                className={'flex flex-col'}>
-                {products.map(p => {
-                    return <Link
-                        key={`${p.name}${p.id}`}
-                        className={'m-3 bg-neutral-400 hover:bg-neutral-200'}
-                        href={`${usePathname()}/${p.id}`}>Product {p.id}</Link>
+            <nav className={'flex flex-col'}>
+                {products.map((p) => {
+                    return (
+                        <Link
+                            key={`${p.name}${p.id}`}
+                            className={'m-3 bg-neutral-400 hover:bg-neutral-200'}
+                            href={`${pathname}/${p.id}`}
+                        >
+                            Product {p.id}
+                        </Link>
+                    );
                 })}
             </nav>
         </>
-
-    )
+    );
 }
