78b27f93a99219412343696b45b0d8d535063c9c
Peter Ramos <86265240+dchamp16@users.noreply.github.com>
2024-08-01 19:43:01 -0600

    Update page.tsx

--- routing/routing-demo/src/app/pages/blog/page.tsx
+++ routing/routing-demo/src/app/pages/blog/page.tsx
@@ -1,43 +1,44 @@
 'use client'
 import { useRouter } from 'next/navigation';
-import {useState} from "react";
+import { useState } from "react";
 
 export default function Blog() {
     const router = useRouter();
-    const [randomNum , setRandomNum] = useState<number>(0);
+    const [randomNum, setRandomNum] = useState<number>(0); // Ensures the initial value is a number
 
-    const getRandomNum = () : number => {
-        const num : number = Math.floor(Math.random() * 10);
+    const getRandomNum = (): any => {
+        const num: number = Math.floor(Math.random() * 10);
         setRandomNum(num);
-        return num
-    }
+        return num; // Ensure a number is always returned
+    };
 
-    if(randomNum === 3) {
-        throw new Error('error loading review')
+    if (randomNum === 3) {
+        throw new Error('error loading review');
     }
-
 
     return (
         <>
             <h1>Blog</h1>
-            <button className={'p-2 border-2 hover:bg-amber-100'} onClick={getRandomNum}>getRandomNum</button>
+            <button className="p-2 border-2 hover:bg-amber-100" onClick={getRandomNum}>
+                getRandomNum
+            </button>
             <ul className="list-disc list-inside">
                 <p>{randomNum}</p>
                 <li
                     className="cursor-pointer text-blue-500 hover:underline"
-                    onClick={() => router.push('pages/blog/first')}
+                    onClick={() => router.push('/pages/blog/first')}
                 >
                     First
                 </li>
                 <li
                     className="cursor-pointer text-blue-500 hover:underline"
                     onClick={() => {
-                        router.push('pages/blog/second');
+                        router.push('/pages/blog/second');
                     }}
                 >
                     Second
                 </li>
             </ul>
         </>
-    )
+    );
 }
