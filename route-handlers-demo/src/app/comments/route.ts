477b85c2e98c0a00546c5f1496468d460cc8cb6b
Peter Ramos <86265240+dchamp16@users.noreply.github.com>
2024-08-15 19:08:37 -0600

    update

---
+++ route-handlers-demo/src/app/comments/route.ts
@@ -0,0 +1,25 @@
+import { comments } from "./data";
+
+export async function GET() {
+  return Response.json(comments, {
+    headers: {
+      "Content-Type": "application/json",
+    },
+    status: 200,
+  });
+}
+
+export async function POST(request: Request) {
+  const comment = await request.json();
+  const newcomment = {
+    id: comments.length + 1,
+    text: comment.text,
+  };
+  comments.push(newcomment);
+  return new Response(JSON.stringify(newcomment), {
+    headers: {
+      "Content-Type": "application/json",
+    },
+    status: 201,
+  });
+}
