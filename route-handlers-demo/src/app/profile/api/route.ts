47275b17a8ae5e6af47bd7fc8302aa081ff0c42b
Peter Ramos <86265240+dchamp16@users.noreply.github.com>
2024-08-15 21:00:55 -0600

    update

--- route-handlers-demo/src/app/profile/api/route.ts
+++ route-handlers-demo/src/app/profile/api/route.ts
@@ -1,3 +1,26 @@
-export async function GET(){
-    return new Response('Profile API data');
+import { type NextRequest } from "next/server";
+import { headers, cookies } from "next/headers";
+
+export async function GET(request: NextRequest) {
+  const requestHeaders = new Headers(request.headers);
+  const headerList = headers();
+
+  cookies().set("resultsPerPage", "20");
+  cookies().set("peter", "lauren");
+
+  console.log(cookies().getAll());
+  cookies().delete("peter");
+  cookies().delete("resultsPerPage");
+
+  const theme = request.cookies.get("theme");
+
+  console.log(theme);
+  console.log(requestHeaders.get("Authorization"));
+  console.log(headerList.get("Authorization"));
+  return new Response("<h1>Profile API data</h1>", {
+    headers: {
+      "Content-Type": "text/html",
+      "Set-Cookie": "theme=dark",
+    },
+  });
 }
